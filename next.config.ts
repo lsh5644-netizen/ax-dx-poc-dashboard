852c352af42773316cd204628bc32b3e6097fd6c
lsh5644-netizen <lsh5644@naver.com>
2026-07-29 16:18:36 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export', // ✨ 'standalone'을 'export'로 변경
+  images: {
+    unoptimized: true, // 정적 내보내기 시 Next Image 최적화 비활성화 필수
+  },
   turbopack: {
     root: process.cwd(),
   },
